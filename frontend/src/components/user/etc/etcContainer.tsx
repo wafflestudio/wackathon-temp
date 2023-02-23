faa1ddf1352ff5f01f274c2d1f6ea735085edb48
panghwi <kanean99@naver.com>
2023-02-23 22:24:17 +0900

    user/etc 칸 css 생성

---
+++ frontend/src/components/user/etc/etcContainer.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import styles from './etcContainer.module.scss';
+import ScreenShot from './screenShot';
+import Money from './money';
+import Level from './level';
+export default function EtcContainer() {
+  return (
+    <div>
+      <ScreenShot></ScreenShot>
+      <Money></Money>
+      <Level></Level>
+    </div>
+  );
+}
