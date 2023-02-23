c07908f5fa215bcea29e1a45f9a47996ca1421c9
panghwi <kanean99@naver.com>
2023-02-23 22:37:21 +0900

    status type status container에서 사용되도록 변경

--- frontend/src/components/status/statusContainer.tsx
+++ frontend/src/components/status/statusContainer.tsx
@@ -1,7 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Status from './status';
+import { StatusType } from '../../lib/types';
+
+const initialStatus: StatusType[] = [
+  {
+    name: 'hungry',
+    status: 0,
+  },
+  {
+    name: 'thirsty',
+    status: 0,
+  },
+  {
+    name: 'clean',
+    status: 0,
+  },
+  {
+    name: 'health',
+    status: 0,
+  },
+];
 
 export default function StatusContainer() {
+  const [status, setStatus] = useState<StatusType[]>(initialStatus);
+
   //status.map
   return (
     <div>
