f5d07a5c9062dd4661651df5c176bf755ea0489c
panghwi <kanean99@naver.com>
2023-02-25 00:45:10 +0900

    status 분기처리 준비 완료

--- frontend/src/components/character/character.tsx
+++ frontend/src/components/character/character.tsx
@@ -2,20 +2,27 @@ import React from 'react';
 import styles from './character.module.scss';
 import { useStatusContext } from '../../context/statusContext';
 import { useParams } from 'react-router-dom';
-import waffleLookingAtMe from '../../resources/waffle_basic.gif';
-import waffleLookingAtYou from '../../resources/waffle_dirty.gif';
+import waffleBasic from '../../resources/waffle_basic.gif';
+import waffleDirty from '../../resources/waffle_dirty.gif';
+import waffleDry from '../../resources/waffle_dry.gif';
+import waffleFeed from '../../resources/waffle_feed_f.gif';
+import waffleHungry from '../../resources/waffle_hungry.gif';
+import waffleSick from '../../resources/waffle_sick.gif';
 
 export default function Character() {
   //여기서 useEffect로 character의 state 갱신해주는게 좋을듯?
   const { user } = useStatusContext();
+  const { status } = useStatusContext();
   const { userId } = useParams();
 
-  console.log(user === Number(userId));
+  console.log(status?.waffle.status.hungry + 'hungry');
+
   let icon = '';
   if (user === Number(userId)) {
-    icon = waffleLookingAtMe;
+    //status에 따라 분기처리
+    icon = waffleBasic;
   } else {
-    icon = waffleLookingAtYou;
+    icon = waffleDirty;
   }
   //if userid !== user back으로 설정
   //  if(status == hungry)
