ab3777399fe45c1c53a90fed7f0c825fa4d93939
panghwi <kanean99@naver.com>
2023-02-24 15:35:49 +0900

    toastify 설치

--- frontend/src/components/user/etc/screenShot.tsx
+++ frontend/src/components/user/etc/screenShot.tsx
@@ -1,6 +1,26 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import styles from './screenShot.module.scss';
+import html2canvas from 'html2canvas';
 
 export default function ScreenShot() {
-  return <button className={styles.box}>screenshot</button>;
+  const myRef = useRef<HTMLDivElement>(null);
+
+  const takeScreenshot = () => {
+    if (myRef.current) {
+      html2canvas(myRef.current).then((canvas) => {
+        const link = document.createElement('a');
+        link.download = 'screenshot.png';
+        link.href = canvas.toDataURL();
+        link.click();
+      });
+    }
+  };
+
+  return (
+    <div ref={myRef}>
+      <button className={styles.box} onClick={takeScreenshot}>
+        스샷
+      </button>
+    </div>
+  );
 }
