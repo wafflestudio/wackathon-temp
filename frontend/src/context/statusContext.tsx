ca6e3ed9e09873b8c3a5dcd4c00ff1d4e95558c1
panghwi <kanean99@naver.com>
2023-02-24 09:54:45 +0900

    status context 생성

--- frontend/src/context/statusContext.tsx
+++ frontend/src/context/statusContext.tsx
@@ -1,1 +1,24 @@
-export {};
+import React, { createContext, useContext, useState } from 'react';
+import { StatusType } from '../lib/types';
+
+type StatusContextType = {
+  status: StatusType[] | undefined;
+};
+
+const StatusContext = createContext<StatusContextType>({} as StatusContextType);
+
+export function StatusProvider({ children }: { children: React.ReactNode }) {
+  const [status, setStatus] = useState<StatusType[] | undefined>();
+
+  return (
+    <StatusContext.Provider
+      value={{
+        status,
+      }}
+    >
+      {children}
+    </StatusContext.Provider>
+  );
+}
+
+export const useStatusContext = () => useContext(StatusContext);
