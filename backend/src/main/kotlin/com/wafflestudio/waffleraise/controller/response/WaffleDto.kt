5cc9b2763778a222f2f11f95aa8c759dd0b4d478
leeeryboy <indiv0227@snu.ac.kr>
2023-02-24 16:04:12 +0900

    feat: Display waffle status til first decimal

--- backend/src/main/kotlin/com/wafflestudio/waffleraise/controller/response/WaffleDto.kt
+++ backend/src/main/kotlin/com/wafflestudio/waffleraise/controller/response/WaffleDto.kt
@@ -2,6 +2,7 @@ package com.wafflestudio.waffleraise.controller.response
 
 import com.wafflestudio.waffleraise.entity.Waffle
 import com.wafflestudio.waffleraise.entity.WaffleStatus
+import kotlin.math.roundToInt
 
 data class WaffleDto(
     val status: WaffleStatus,
@@ -10,7 +11,12 @@ data class WaffleDto(
     companion object {
         fun of(waffle: Waffle): WaffleDto {
             return WaffleDto(
-                status = waffle.status,
+                status = WaffleStatus(
+                    hungry = String.format("%.1f", waffle.status.hungry).toDouble(),
+                    thirsty = String.format("%.1f", waffle.status.thirsty).toDouble(),
+                    cleanliness = String.format("%.1f", waffle.status.cleanliness).toDouble(),
+                    health = String.format("%.1f", waffle.status.health).toDouble()
+                ),
                 level = waffle.level / 50
             )
         }
