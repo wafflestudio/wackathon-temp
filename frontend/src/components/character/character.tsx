b3ae469b0a66c276a8f6da133256ae619341f0f9
panghwi <kanean99@naver.com>
2023-02-25 00:35:27 +0900

    waffle character temp 추가

--- frontend/src/components/character/character.tsx
+++ frontend/src/components/character/character.tsx
@@ -2,13 +2,21 @@ import React from 'react';
 import styles from './character.module.scss';
 import { useStatusContext } from '../../context/statusContext';
 import { useParams } from 'react-router-dom';
+import waffleLookingAtMe from '../../resources/testwaffle.svg';
+import waffleLookingAtYou from '../../resources/tesetwaffle2.svg';
+
 export default function Character() {
   //여기서 useEffect로 character의 state 갱신해주는게 좋을듯?
   const { user } = useStatusContext();
   const { userId } = useParams();
 
   console.log(user === Number(userId));
-  //let icon = ''
+  let icon = '';
+  if (user === Number(userId)) {
+    icon = waffleLookingAtMe;
+  } else {
+    icon = waffleLookingAtYou;
+  }
   //if userid !== user back으로 설정
   //  if(status == hungry)
   // icon = hungryIcon
@@ -18,7 +26,7 @@ export default function Character() {
   // if status == hungry
   return (
     <div className={styles.container}>
-      <div className={styles.character}>character</div>
+      <img src={icon}></img>
     </div>
   );
 }
