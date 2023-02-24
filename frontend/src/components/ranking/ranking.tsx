f3793b742926f7aad12255703e19d9fbc49a078d
panghwi <kanean99@naver.com>
2023-02-24 21:42:58 +0900

    ranking css 수정

--- frontend/src/components/ranking/ranking.tsx
+++ frontend/src/components/ranking/ranking.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { RankingType } from '../../lib/types';
 import styles from './ranking.module.scss';
+import profile from '../../resources/profile.svg';
 
 type RankingComponentType = {
   detail: RankingType;
@@ -10,6 +11,7 @@ export default function Ranking({ detail }: RankingComponentType) {
   console.log(detail);
   return (
     <div className={styles.container}>
+      <img src={profile} className={styles.img}></img>
       <div className={styles.name}>{detail.username}</div>
       <div
         className={styles.contribute}
