62e3711f15ed2a17562629b0d8beddfd295d0175
panghwi <kanean99@naver.com>
2023-02-25 13:27:06 +0900

    level 추가

--- frontend/src/components/pages/MainPage.tsx
+++ frontend/src/components/pages/MainPage.tsx
@@ -3,10 +3,12 @@ import Character from '../character/character';
 import ActionButtons from '../actionButtons/actionButtons';
 import PageTemplate from '../pageTemplate';
 import styles from './MainPage.module.scss';
+import Level from '../user/etc/level';
 export default function MainPage() {
   return (
     <PageTemplate>
       <div className={styles.blank}></div>
+      <Level></Level>
       <Character></Character>
       <ActionButtons></ActionButtons>
     </PageTemplate>
--- frontend/src/components/user/etc/level.tsx
+++ frontend/src/components/user/etc/level.tsx
@@ -1,6 +1,13 @@
 import React from 'react';
 import styles from './level.module.scss';
+import level1 from '../../../resources/level1.svg';
+import level2 from '../../../resources/level2.svg';
+import { useSearchParams } from 'react-router-dom';
+import { useStatusContext } from '../../../context/statusContext';
 
 export default function Level() {
-  return <div className={styles.box}>level</div>;
+  const { status } = useStatusContext();
+  console.log(status?.waffle.level);
+
+  return <img className={styles.img} src={level1}></img>;
 }
