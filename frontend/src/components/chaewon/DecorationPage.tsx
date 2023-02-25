7772f5451f42dba9a2d02645a98b6f2fc5e42e6f
panghwi <kanean99@naver.com>
2023-02-25 14:34:17 +0900

    데코기능 페이지 수정

--- frontend/src/components/chaewon/DecorationPage.tsx
+++ frontend/src/components/chaewon/DecorationPage.tsx
@@ -1,21 +1,27 @@
-import React from "react";
-import Decolist from "../../resources/decolist.png";
-import DecoratedWaffle from "../../resources/decoratedwaffle.gif";
+import React from 'react';
+import Decolist from '../../resources/decolist.png';
+import DecoratedWaffle from '../../resources/decoratedwaffle.gif';
 
-import PageTemplate from "../pageTemplate";
-import styles from "./DecorationPage.module.scss";
+import PageTemplate from '../pageTemplate';
+import styles from './DecorationPage.module.scss';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 export default function DecorationPage() {
+  const message = () => {
+    toast.warn('데코 기능 사용을 위해선 국민93770200501153');
+  };
   return (
     <PageTemplate>
       <div className={styles.wrapper}>
         <div className={styles.imgWrapper}>
-          <img src={DecoratedWaffle} alt=""></img>
+          <img src={DecoratedWaffle} alt=''></img>
         </div>
         <div className={styles.listWrapper}>
-          <img src={Decolist} alt=""></img>
+          <img src={Decolist} alt='' onClick={message}></img>
         </div>
       </div>
+      <ToastContainer />
     </PageTemplate>
   );
 }
